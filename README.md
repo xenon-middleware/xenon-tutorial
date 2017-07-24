80c4b9ec63e1d1615720759df5b14368d68711a8
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2017-07-24 16:59:16 +0200

    Added installation steps

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # xenon-rse2017-tutorial
-Material for Xenon tutorial at http://rse.ac.uk/conf2017/
+
+Material for [Xenon](http://nlesc.github.io/Xenon/) tutorial for [Research Software Engineers Conference 2017](http://rse.ac.uk/conf2017/).
+
+## Installation
+
+Installation of tutorial material in tutorial virtual machine.
+
+```bash
+cd xenon
+git clone https://github.com/NLeSC/xenon-rse2017-tutorial.git .
+install.sh
+```
+
+After which 
+* the [Xenon cli](https://github.com/NLeSC/xenon-cli) has been installed as `~/xenon/xenon-cli/bin/xenon`.
+* Slurm batch scheduler is runnin as Docker container, login with `ssh -p 2222 xenon@localhost` and password `javagat`.
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/sh
+
+# install docker
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf makecache fast
+sudo dnf install -y docker-ce
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# install docker-compose
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+
+# start slurm batch scheduler Docker container
+docker-compose up -d
+
+# download/untar van xenon-cli
+wget https://github.com/NLeSC/xenon-cli/releases/download/v1.0.3/xenon-cli-shadow-1.0.3.tar
+tar -xf xenon-cli-shadow-*.tar
+rm *.tar
+mv xenon-cli-shadow* xenon-cli
+
+# put xenon-cli/bin in PATH at login
+echo 'export PATH=$PATH:$HOME/xenon/xenon-cli/bin' > .bashrc
