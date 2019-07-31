215a2fbf911cc421d80e325324ffb65d41d41aea
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2019-07-31 16:49:54 +0200

    updated file locations and ssh://

--- readthedocs/code-tabs/bash/AllTogetherNowWrong.sh
+++ readthedocs/code-tabs/bash/AllTogetherNowWrong.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # step 1: upload input file(s)
-xenon filesystem sftp --location localhost:10022 --username xenon --password javagat \
-upload /home/tutorial/xenon/sleep.sh /home/xenon/sleep.sh
+xenon filesystem sftp --location ssh://localhost:10022 --username xenon --password javagat \
+upload /home/alice/sleep.sh /home/xenon/sleep.sh
 
 # step 2: submit job
-xenon scheduler slurm --location localhost:10022 --username xenon --password javagat \
+xenon scheduler slurm --location ssh://localhost:10022 --username xenon --password javagat \
 submit --stdout sleep.stdout.txt bash sleep.sh 60
 
 # step 3: download generated output file(s)
-xenon filesystem sftp --location localhost:10022 --username xenon --password javagat \
-download /home/xenon/sleep.stdout.txt /home/tutorial/xenon/sleep.stdout.txt
+xenon filesystem sftp --location ssh://localhost:10022 --username xenon --password javagat \
+download /home/xenon/sleep.stdout.txt /home/alice/sleep.stdout.txt
--- readthedocs/code-tabs/bash/sleep.sh
+++ readthedocs/code-tabs/bash/sleep.sh
@@ -1,6 +1,4 @@
-.. code-block:: bash
-
-    #!/usr/bin/env bash
-    echo `date`': went to bed'
-    sleep $1
-    echo `date`': woke up'
+#!/usr/bin/env bash
+echo `date`': went to bed'
+sleep $1
+echo `date`': woke up'
