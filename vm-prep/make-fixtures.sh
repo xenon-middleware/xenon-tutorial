a561209cbc2ac179f0db33019649b43ba89e41b2
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2019-08-02 13:53:22 +0200

    trying out making fixtures on travis with local paths

--- vm-prep/make-fixtures.sh
+++ vm-prep/make-fixtures.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
 # files and directories for making directory listings
-mkdir /home/travis/fixtures/dir1
-mkdir /home/travis/fixtures/.dir2
-echo 'dir1/file1.txt' > /home/travis/fixtures/dir1/file1.txt
-echo 'dir1/.file2.txt' > /home/travis/fixtures/dir1/.file2.txt
-echo '.dir2/file3.txt' > /home/travis/fixtures/.dir2/file3.txt
-echo '.dir2/.file4.txt' > /home/travis/fixtures/.dir2/.file4.txt
+mkdir fixtures/dir1
+mkdir fixtures/.dir2
+echo 'dir1/file1.txt' > fixtures/dir1/file1.txt
+echo 'dir1/.file2.txt' > fixtures/dir1/.file2.txt
+echo '.dir2/file3.txt' > fixtures/.dir2/file3.txt
+echo '.dir2/.file4.txt' > fixtures/.dir2/.file4.txt
 
 # file for copying locally
-echo 'some content' > /home/travis/thefile.txt
+echo 'some content' > thefile.txt
 
 # sleep script
-echo '#!/usr/bin/env bash' > /home/travis/sleep.sh
-echo 'echo `date`: went to bed' >> /home/travis/sleep.sh
-echo 'sleep $1' >> /home/travis/sleep.sh
-echo 'echo `date`: woke up' >> /home/travis/sleep.sh
+echo '#!/usr/bin/env bash' > sleep.sh
+echo 'echo `date`: went to bed' >> sleep.sh
+echo 'sleep $1' >> sleep.sh
+echo 'echo `date`: woke up' >> sleep.sh
+
