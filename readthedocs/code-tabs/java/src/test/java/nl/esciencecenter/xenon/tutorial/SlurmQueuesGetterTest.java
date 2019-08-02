9816c39e05e3381a8871ddc1013ba159dc71eeab
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2019-08-02 13:05:20 +0200

    updated test with example that uses testcontainers lib

--- readthedocs/code-tabs/java/src/test/java/nl/esciencecenter/xenon/tutorial/SlurmQueuesGetterTest.java
+++ readthedocs/code-tabs/java/src/test/java/nl/esciencecenter/xenon/tutorial/SlurmQueuesGetterTest.java
@@ -1,13 +1,36 @@
 package nl.esciencecenter.xenon.tutorial;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+
 
 
 public class SlurmQueuesGetterTest {
 
+    private String host;
+    private String port;
+    private Map<String, String> properties;
+
+    @Rule
+    public GenericContainer<?> slurm = new GenericContainer<>("nlesc/xenon-slurm:17").withExposedPorts(22);
+
+    @Before
+    public void setUp() {
+        host = slurm.getContainerIpAddress();
+        port = Integer.toString(slurm.getFirstMappedPort());
+        properties = new HashMap<String, String>();
+        properties.put("xenon.adaptors.schedulers.ssh.loadSshConfig", "false");
+        properties.put("xenon.adaptors.schedulers.ssh.loadKnownHosts", "false");
+    }
+
     @Test(expected = Test.None.class)
     public void test1() throws Exception {
-        SlurmQueuesGetter.main(null);
+        SlurmQueuesGetter.main(host, port, properties);
     }
 
 }
