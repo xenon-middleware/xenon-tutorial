2162392090785bbdb441db3f2626e6d0765f3477
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2019-08-05 18:20:17 +0200

    conf updated

--- readthedocs/code-tabs/python/setup.py
+++ readthedocs/code-tabs/python/setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=readme + '\n\n',
     author="Jurriaan H. Spaaks",
     author_email='j.spaaks@esciencecenter.nl',
-    url='https://github.com/xenon-middleware/pyxenon_snippets',
+    url='https://github.com/xenon-middleware/xenon-tutorial',
     packages=[
         'pyxenon_snippets',
     ],
@@ -43,18 +43,11 @@ setup(
     install_requires=[],  # FIXME: add your package's dependencies to this list
     setup_requires=[
         # dependency for `python setup.py test`
-        'pytest-runner',
-        # dependencies for `python setup.py build_sphinx`
-        'sphinx',
-        'sphinx_rtd_theme',
-        'recommonmark'
+        'pytest-runner'
     ],
     tests_require=[
         'pytest',
         'pytest-cov',
-        'pycodestyle',
-    ],
-    extras_require={
-        'dev':  ['prospector[with_pyroma]', 'yapf', 'isort'],
-    }
+        'pycodestyle'
+    ]
 )
