5f1cadaccea7f0feb4c8943bc02a6c2794df3d86
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2019-08-07 15:13:35 +0200

    travis config

--- readthedocs/code-tabs/python/pyxenon_snippets/slurm_queues_getter.py
+++ readthedocs/code-tabs/python/pyxenon_snippets/slurm_queues_getter.py
@@ -1,32 +1,8 @@
-import subprocess
-
 import xenon
 from xenon import Scheduler, PasswordCredential
-from xenon.compat import find_xenon_grpc_jar
-from xenon.create_keys import create_self_signed_cert
 
 
 def run_example():
-
-    jar_file = find_xenon_grpc_jar()
-    if not jar_file:
-        raise RuntimeError("Could not find 'xenon-grpc' jar file.")
-
-    cmd = ['java', '-jar', jar_file, '-vvvvvv']
-
-    crt_file, key_file = create_self_signed_cert()
-
-    cmd.extend([
-        '--server-cert-chain', str(crt_file),
-        '--server-private-key', str(key_file),
-        '--client-cert-chain', str(crt_file)])
-
-    subprocess.Popen(
-        cmd,
-        bufsize=1,
-        universal_newlines=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
 
     xenon.init()
 
--- readthedocs/code-tabs/python/tests/test_slurm_queues_getter.py
+++ readthedocs/code-tabs/python/tests/test_slurm_queues_getter.py
@@ -4,7 +4,6 @@ import pytest
 
 from pyxenon_snippets import slurm_queues_getter
 
-
 def test_slurm_queues_getter():
     slurm_queues_getter.run_example()
 
