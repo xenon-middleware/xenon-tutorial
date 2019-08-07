4a835713670fac06ef436069ea1de151b1e0a5ce
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2019-08-07 19:37:29 +0200

    no need for prop if i rm the .ssh; less verbose to avoid clutter

--- readthedocs/code-tabs/bash/SlurmQueuesGetter.sh
+++ readthedocs/code-tabs/bash/SlurmQueuesGetter.sh
@@ -1,4 +1,4 @@
-xenon -vvvv scheduler slurm --location ssh://localhost:10022 --username xenon --password javagat --prop xenon.adaptors.schedulers.ssh.loadKnownHosts=false queues
+xenon -vvv scheduler slurm --location ssh://localhost:10022 --username xenon --password javagat queues
 # returns:
 # Available queues: mypartition, otherpartition
 # Default queue: mypartition
--- readthedocs/code-tabs/bash/travis.sh
+++ readthedocs/code-tabs/bash/travis.sh
@@ -4,6 +4,7 @@
 
 run_snippet () {
    echo "travis_fold:start:$1"
+   echo "[output of $1]"
    bash $1 || exit 1; 
    echo "travis_fold:end:$1"
 }
