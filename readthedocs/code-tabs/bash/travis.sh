b40be27543e06427ca0af15ede1e5a824a4132bc
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2019-08-07 19:53:23 +0200

    travis config

--- readthedocs/code-tabs/bash/travis.sh
+++ readthedocs/code-tabs/bash/travis.sh
@@ -5,21 +5,22 @@
 run_snippet () {
    echo "travis_fold:start:$1"
    echo "[output of $1]"
-   #bash $1 || exit 1; 
-   bash $1
+   bash $1 || exit 1; 
    echo "travis_fold:end:$1"
 }
 
-snippets="DirectoryListing.sh \
-          DirectoryListingShowHidden.sh \
-          DirectoryListingRecursive.sh \
-          CopyFileLocalToLocalAbsolutePaths.sh \
-          SlurmQueuesGetter.sh \
-          UploadFileLocalToSftpAbsolutePaths.sh \
-          SlurmJobListGetter.sh \
-          DownloadFileSftpToLocalAbsolutePaths.sh \
-          AllTogetherNowWrong.sh \
-          AllTogetherNow.sh"
+# snippets="DirectoryListing.sh \
+#           DirectoryListingShowHidden.sh \
+#           DirectoryListingRecursive.sh \
+#           CopyFileLocalToLocalAbsolutePaths.sh \
+#           SlurmQueuesGetter.sh \
+#           UploadFileLocalToSftpAbsolutePaths.sh \
+#           SlurmJobListGetter.sh \
+#           DownloadFileSftpToLocalAbsolutePaths.sh \
+#           AllTogetherNowWrong.sh \
+#           AllTogetherNow.sh"
+
+snippets="SlurmQueuesGetter.sh"
 
 for snippet in $snippets ; 
 do
