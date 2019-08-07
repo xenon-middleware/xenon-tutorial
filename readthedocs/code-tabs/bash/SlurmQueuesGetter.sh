09514c26da34a3d0e7dbea65aa06b511386998f3
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2019-08-07 18:58:36 +0200

    readded prop

--- readthedocs/code-tabs/bash/SlurmQueuesGetter.sh
+++ readthedocs/code-tabs/bash/SlurmQueuesGetter.sh
@@ -1,4 +1,4 @@
-xenon -vvvvv scheduler slurm --location ssh://localhost:10022 --username xenon --password javagat queues
+xenon -vvvvv scheduler slurm --location ssh://localhost:10022 --username xenon --password javagat --prop xenon.adaptors.schedulers.ssh.loadKnownHosts=false queues
 # returns:
 Available queues: mypartition, otherpartition
 Default queue: mypartition
