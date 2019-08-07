991acb1eee3a07ce91d91e1d581b6aa9ea3e3845
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2019-08-07 17:32:58 +0200

    bash on travis

--- readthedocs/code-tabs/bash/travis.sh
+++ readthedocs/code-tabs/bash/travis.sh
@@ -3,16 +3,14 @@
 # some examples need a running docker, see .travis.yml
 
 bash DirectoryListing.sh || exit 1; 
-# bash DirectoryListingShowHidden.sh || exit 1;
-# bash DirectoryListingRecursive.sh || exit 1;
-# bash CopyFileLocalToLocalAbsolutePaths.sh || exit 1;
-# bash SlurmQueuesGetter.sh || exit 1;
-# bash UploadFileLocalToSftpAbsolutePaths.sh || exit 1;
-# bash SlurmJobListGetter.sh || exit 1;
-# bash DownloadFileSftpToLocalAbsolutePaths.sh || exit 1;
-# bash AllTogetherNowWrong.sh || exit 1;
-# bash AllTogetherNow.sh || exit 1;
-# 
-# 
-# 
+bash DirectoryListingShowHidden.sh || exit 1;
+bash DirectoryListingRecursive.sh || exit 1;
+bash CopyFileLocalToLocalAbsolutePaths.sh || exit 1;
+bash SlurmQueuesGetter.sh || exit 1;
+bash UploadFileLocalToSftpAbsolutePaths.sh || exit 1;
+bash SlurmJobListGetter.sh || exit 1;
+bash DownloadFileSftpToLocalAbsolutePaths.sh || exit 1;
+bash AllTogetherNowWrong.sh || exit 1;
+bash AllTogetherNow.sh || exit 1;
+ 
 exit 0;
