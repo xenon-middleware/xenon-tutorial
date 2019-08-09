1b5035ca96959425817b5929fef8955772c1aaa3
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2019-08-09 14:50:25 +0200

    webdav equivalent to sftp copy

---
+++ readthedocs/code-tabs/java/src/main/java/nl/esciencecenter/xenon/tutorial/UploadFileLocalToSftpAbsolutePathsWebdavEquivalent.java
@@ -0,0 +1,66 @@
+package nl.esciencecenter.xenon.tutorial;
+
+import java.util.Map;
+
+import nl.esciencecenter.xenon.XenonException;
+import nl.esciencecenter.xenon.credentials.PasswordCredential;
+import nl.esciencecenter.xenon.filesystems.CopyMode;
+import nl.esciencecenter.xenon.filesystems.CopyStatus;
+import nl.esciencecenter.xenon.filesystems.FileSystem;
+import nl.esciencecenter.xenon.filesystems.Path;
+
+public class UploadFileLocalToSftpAbsolutePathsWebdavEquivalent {
+
+
+    public static void main(String[] args) throws XenonException {
+
+        String host = "localhost";
+        String port = "10080";
+        Map<String, String> properties = null;
+
+        runExample(host, port, properties);
+    }
+
+    public static void runExample(String host, String port, Map<String, String> properties) throws XenonException {
+
+        // use the local file system adaptor to create a file system representation
+        String adaptorLocal = "file";
+        FileSystem filesystemLocal = FileSystem.create(adaptorLocal);
+
+        // define what file to upload
+        Path fileLocal = new Path("/home/daisycutter/tmp/home/travis/sleep.sh");
+
+        // use the webdav file system adaptor to create a file system representation; the remote
+        // filesystem requires credentials to log in, so we'll have to create those too.
+        String username = "xenon";
+        char[] password = "javagat".toCharArray();        
+        PasswordCredential credential = new PasswordCredential(username, password);        
+
+        String adaptorRemote = "webdav";
+        String location = "http://" + host + ":" + port;
+
+        FileSystem filesystemRemote = FileSystem.create(adaptorRemote, location, credential, properties);
+
+        // define which file to upload to
+        Path fileRemote = new Path("/~xenon/uploads/sleep.sh");
+
+        // create the destination file only if the destination path doesn't exist yet
+        CopyMode mode = CopyMode.CREATE;
+
+        // no need to recurse, we're just downloading a file
+        boolean recursive = false;
+
+        // perform the copy/upload and wait 1000 ms for the successful or otherwise
+        // completion of the operation
+        String copyId = filesystemLocal.copy(fileLocal, filesystemRemote, fileRemote, mode, recursive);
+        long timeoutMilliSecs = 1000;
+        CopyStatus copyStatus = filesystemLocal.waitUntilDone(copyId, timeoutMilliSecs);
+
+        // print any exceptions
+        if (copyStatus.getException() != null) {
+            System.out.println(copyStatus.getException().getMessage());
+        } else {
+            System.out.println("Done");
+        }
+    }
+}
